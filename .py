4a24f0183a6f2bf3d22a258b8ceefdb3f54fe3e9
NANO_Prime(HALİM) <129737308+halim956@users.noreply.github.com>
2024-08-07 21:24:34 +0300

    Update .py

--- .py
+++ .py
@@ -12,7 +12,7 @@ async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
 @bot.command()
-async def hello(ctx):
+async def merhaba(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
 @bot.command()
